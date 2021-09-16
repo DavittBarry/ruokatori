d88a3f5556f1a0137ce44866101d889557238a06
Davitt Barry <63282613+DavittBarry@users.noreply.github.com>
2021-09-16 14:27:12 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
+// Initialize Bootstrap tooltips
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl);
 });
 
+// Initialize Bootstrap popovers
 var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
-var popoverList = tooltipTriggerList.map(function (popoverTriggerEl) {
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
     return new bootstrap.Popover(popoverTriggerEl);
 });
