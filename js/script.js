9ee6e2052267f90a8e170228caa8990f637846fe
Aleksi Utriainen <75065826+PasilanPrinssi@users.noreply.github.com>
2021-09-23 17:17:29 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,6 @@
-// Initialize Bootstrap tooltips
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl);
-});
+var menuBtn = document.getElementById('menuBtn');
 
-// Initialize Bootstrap popovers
-var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
-var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-    return new bootstrap.Popover(popoverTriggerEl);
-});
+menuBtn.onclick = function () {
+  document.body.classList.toggle('menu-open');
+};
+
